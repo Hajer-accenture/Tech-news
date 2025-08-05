12aa5b59726cf52d7446b98368838f44853d4d33
Hammouda <hammoudahajer8@gmail.com>
2025-08-05 11:42:38 +0200

    cleaning files

--- tasks/fetch_task.py
+++ tasks/fetch_task.py
@@ -1,6 +1,6 @@
 from crewai import Task
 
 fetch_task = Task(
-    description="Fetch today's trending technology news articles.",
-    expected_output="List of 5 headline+link pairs",
+    description="Fetch today's trending technology news articles",
+    expected_output="List of 5 top relevant headline+link pairs",
 )
