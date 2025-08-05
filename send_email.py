4442ce02756a150c30f93d64c14d20ec2dddde69
Hammouda <hammoudahajer8@gmail.com>
2025-08-05 17:18:46 +0200

    Change content in send Email

--- send_email.py
+++ send_email.py
@@ -1,6 +1,8 @@
 import os
 import sendgrid
 from sendgrid.helpers.mail import Mail, Email, To, Content
+from sympy import content
+
 
 def send_email(subject, body):
     api_key = os.environ.get("SendGrid_API_KEY")
@@ -10,7 +12,8 @@ def send_email(subject, body):
     sg = sendgrid.SendGridAPIClient(api_key=api_key)
     from_email = Email(email_from)
     to_email = To(email_to)
-    mail = Mail(from_email, to_email, subject, body)
+    content = Content("text/plain", body)
+    mail = Mail(from_email, to_email, subject, content)
 
     # Get a JSON-ready representation of the Mail object
     mail_json = mail.get()
