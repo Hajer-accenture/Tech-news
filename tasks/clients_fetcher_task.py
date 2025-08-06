4627f8ee0e853fe069c34f98fed21864d0e834b7
Hammouda <hammoudahajer8@gmail.com>
2025-08-06 18:17:02 +0200

    changing prompt

--- tasks/clients_fetcher_task.py
+++ tasks/clients_fetcher_task.py
@@ -7,12 +7,12 @@ companies = [
 
 fetch_clients_task = Task(
     description=(
-        f"Search and summarize the most recent and relevant technology-related news (past 2–3 days) "
+        f"Search and summarize the most recent and relevant news (past 2–3 days) of 2025 "
         f"for the following companies: {', '.join(companies)}. "
         "Focus on news related to innovation, digital transformation, R&D, AI, cybersecurity, electrification, or other emerging technologies. "
         "Focus also on financial or related political news."
     ),
     expected_output=(
-        "A list of bullet points (3–4 per company) summarizing key tech-related, financial and political political with publication date"
+        "A list of bullet points (3–4 per company) summarizing key financial, political and tech-related news from the past 2–3 days (today is August 2025)"
     )
 )
