c90605cb60192129a5d1e4b852b6c04191a36ebe
Hammouda <hammoudahajer8@gmail.com>
2025-08-06 12:45:08 +0200

    Debugging sendgrid

--- send_email.py
+++ send_email.py
@@ -5,10 +5,9 @@ from sendgrid.helpers.mail import Mail, Email, To, Content
 
 
 def send_email(subject, body):
-    api_key = os.environ.get("SENGRID_API_KEY")
+    api_key = os.environ.get("SENDGRID_API_KEY")
     email_to = os.environ.get("EMAIL_TO")
     email_from = os.environ.get("EMAIL_FROM")
-
     sg = sendgrid.SendGridAPIClient(api_key=api_key)
     from_email = Email(email_from)
     to_email = To(email_to)
