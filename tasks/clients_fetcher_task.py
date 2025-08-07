0634ceb2e3aa8217c22441fc7e23c32332f0e61d
Hammouda <hammoudahajer8@gmail.com>
2025-08-07 17:07:41 +0200

    Fetcher changes

--- tasks/clients_fetcher_task.py
+++ tasks/clients_fetcher_task.py
@@ -10,10 +10,10 @@ companies = [
 fetch_clients_task = Task(
     description=(
         f"Collect all recent concrete and verifiable company news that could significantly affect a company’s strategic trajectory"
-        "published within the last month from {datetime.today()}"
+        f"published within the last month from {datetime.today()}"
         f"for the following companies: {', '.join(companies)}. "
         "Focus on high-impact developments related to innovation, digital transformation, R&D, AI, or other emerging technologies,"
-        "financial events, executive leadership, or political implications"
+        "finafncial events, executive leadership, or political implications"
     ),
     expected_output=(
         "For each company, a list of 4–5 detailed and recent news items. for each company, each entry must include the headline, summary, date, and source"
